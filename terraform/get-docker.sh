25c5915b4bfe85e5c3e2ffc2a86ba71abccbc3db
MaheshBabu-DevOps <maheshtemmanaboina@gmail.com>
2025-07-08 10:53:51 +0530

    Banking-project

--- terraform/get-docker.sh
+++ terraform/get-docker.sh
@@ -19,7 +19,7 @@ set -e
 #   Docker Buildx, Docker Compose, containerd, and runc. When using this script
 #   to provision a machine, this may result in unexpected major version upgrades
 #   of these packages. Always test upgrades in a test environment before
-#   deploying to your production systems.
+#   deploying to your production env systems.
 # - Isn't designed to upgrade an existing Docker installation. When using the
 #   script to update an existing installation, dependencies may not be updated
 #   to the expected version, resulting in outdated versions.
